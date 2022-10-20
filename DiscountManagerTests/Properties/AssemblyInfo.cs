72ca8a879a39570d3e475e62246e6733e2235dbe
Andrea Vallati <an.vallati@gmail.com>
2022-10-20 22:23:30 +0200

    Updated Unit Tests

--- DiscountManagerTests/Properties/AssemblyInfo.cs
+++ DiscountManagerTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("DiscountManagerUnitTests")]
+[assembly: AssemblyTitle("DiscountManagerTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiscountManagerUnitTests")]
+[assembly: AssemblyProduct("DiscountManagerTests")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("582ef9bf-9870-4fc7-ad64-26b98469a27d")]
+[assembly: Guid("561f7799-e93a-4d23-91bc-f05c236e4164")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
